75a8f250dd405a454ce93927afc06273b59c8264
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-06-03 15:38:37 -0600

    funcion post ¨ guardar todos¨

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-let ingresar = document.getElementById("ingresar")
-let agregar = document.getElementById("agregar")
-let btnTarea = document.getElementById("tarea")
+let ingresar = document.getElementById("ingresar");
+let btnAgregar = document.getElementById("btn-agregar");
+let btneliminar = document.getElementById("btn-eliminar");
+
+//METODO POST
+async function guardarDatos() {
+  try {
+    let tarea = {
+      id: Date.now(),
+      nombre: "TAREA",
+      estado: false
+    } 
+    const respuesta = await fetch("http://localhost:3000/api/task",{
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify(tarea)
+    })
+    let datos = await respuesta.json()
+    console.log(datos);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+btnAgregar.addEventListener("click",guardarDatos)
 
 
-ingresar.addEventListener("click", function () {
-    
+/*METODO GET*/
 
-})
-
-
-
-
-
-
-
-
+async function datos(){
+  try {
+      const respuesta = await fetch("http://localhost:3000/api/task")
+      const datos = await respuesta.json()
+      console.log(datos);
+  } catch (error) {
+   console.error(error);   
+  }
+}

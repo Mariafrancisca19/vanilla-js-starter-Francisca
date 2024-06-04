0804ceb31efe2e4a13e8f950cf8cfe9471f5e985
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-06-04 15:49:43 -0600

    funcion para guardar las tareas

---
+++ src/JS/checkbox.js
@@ -0,0 +1,28 @@
+/*const checkBox = document.querySelector("#checkbox");
+
+function listaTareas() {
+    let item = document.getElementById("checkbox").value
+    let text = document.createTextNode(item)
+    let nuevaTrea = document.createElement("input")
+    checkboxmaker. type = "checkbox"
+    checkboxmaker.name = "name"
+    checkboxmaker.value = "value"
+    checkboxmaker.id = "id"
+    checkboxmaker.classList = "corrected"
+
+    nuevaTrea.appendChild(text);
+    nuevaTrea.appendChild(checkboxmaker);
+    document.getElementById("lista").appendChild(nuevaTrea);
+}
+
+    checkBox.addEventListener("click",listaTareas);
+
+    const itemLista = document.querySelector("elementoLista")
+
+    function marcador() {
+        itemLista = document. toggle("donete")
+    }
+
+    itemLista.addEventListener("click",marcador)*/
+
+/*METODO GET*/
--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 let ingresar = document.getElementById("ingresar");
 let btnAgregar = document.getElementById("btn-agregar");
 let btneliminar = document.getElementById("btn-eliminar");
+let lista = document.getElementById("lista")
+
+/*METODO GET*/
+
+async function datos(){
+  try {
+    lista.innerHTML=""
+      const respuesta = await fetch("http://localhost:3000/api/task")
+      const datos = await respuesta.json() 
+      datos.forEach(tarea=>{
+        let p = document.createElement("li")
+        p.innerHTML=tarea.nombre
+        lista.appendChild(p)
+
+         let contenedor = document.createElement("div")
+        //  contenedor.innerHTML = tarea.nombre
+        //  lista.appendChild(contenedor)
+
+         let checkBox = document.createElement("checkbox")
+         checkBox.innerHTML = tarea.nombre
+         lista.appendChild(contenedor)
+
+
+      })
+      console.log(datos);
+  } catch (error) {
+   console.error(error);   
+  }
+}
 
 //METODO POST
 async function guardarDatos() {
   try {
     let tarea = {
       id: Date.now(),
-      nombre: "TAREA",
+      nombre: ingresar.value,
       estado: false
     } 
     const respuesta = await fetch("http://localhost:3000/api/task",{
@@ -17,24 +46,32 @@ async function guardarDatos() {
       },
       body: JSON.stringify(tarea)
     })
-    let datos = await respuesta.json()
-    console.log(datos);
+    let datosG = await respuesta.json()
+    console.log(datosG);
+    datos()
   } catch (error) {
     console.error(error);
   }
 }
 
-btnAgregar.addEventListener("click",guardarDatos)
+btnAgregar.addEventListener("click", () => {
+  guardarDatos()
+})
 
 
-/*METODO GET*/
 
-async function datos(){
-  try {
-      const respuesta = await fetch("http://localhost:3000/api/task")
-      const datos = await respuesta.json()
-      console.log(datos);
-  } catch (error) {
-   console.error(error);   
-  }
-}
+/*POST*/
+
+
+
+
+/*PUT*/
+
+
+
+
+/*DELETE*/
+
+
+
+

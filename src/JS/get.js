9f3fb01c8753093f1f7fafee7e468a788e28c91a
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-06-07 15:54:49 -0600

    Mensaje de no hay tareas

--- src/JS/get.js
+++ src/JS/get.js
@@ -1,15 +1,42 @@
 import { actualizar } from "./put"
 import { contadorTareas } from ".."
 import { eliminarTarea } from "./delete"
+import Swal from 'sweetalert2'
+
+
+let texto = document.getElementById("nada")
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'center',
+    iconColor: 'white',
+    customClass: {
+      popup: 'colored-toast',
+    },
+    showConfirmButton: false,
+    timer: 20000,
+    timerProgressBar: true,
+  })
+
 /*METODO GET:Retorna todas las tareas*/
 async function datos(){ 
     try {
       lista.innerHTML=""
       const respuesta = await fetch("http://localhost:3000/api/task")
       const datos = await respuesta.json() 
+      let sinTaeras = Array.from(datos)
       datos.forEach(tarea=>{
         let p = document.createElement("li")
+        p.classList.add("square")
         p.innerHTML=tarea.nombre
+
+        if (sinTaeras.length==0) {
+            texto.style.display= "block"
+            
+        } else { 
+            texto.style.display="none"
+            
+        }
         
         let contenedor = document.createElement("div")
         let checkBox = document.createElement("input")
@@ -19,9 +46,15 @@ async function datos(){
         let botonEliminar = document.createElement("button") //Funcion para el boton elimiar
         botonEliminar.innerHTML = "🗑️"
         
-        botonEliminar.addEventListener("click",()=>{
+        botonEliminar.addEventListener("click",async()=>{
           //FUNCION DELETE
           eliminarTarea(tarea.id)
+          await Toast.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })    
           })
           
           checkBox.checked = tarea.estado
